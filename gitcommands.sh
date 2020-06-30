2fd248bbfd577b3ffa2edc6670f289d7e6799520
Michelle Ma <michelle.ma@tuftsceeo.org>
2020-06-30 14:23:27 -0400

    edits

--- gitcommands.sh
+++ gitcommands.sh
@@ -1,5 +1,5 @@
 # ONLY COMMITS CSS AND HTML 
 # MAKE ANOTHER SCRIPT FOR JS 
-git add public/stylesheets/style.css views/index.html app.js image/CAD.gif image/sketch1.gif
+git add public/stylesheets/style.css views/index.html app.js image/CAD.gif image/sketch1.gif public/stylesheets/LandasansUltraLight-qZYq5.ttf public/stylesheets/LemonMilkMedium-mLZYV.otf 
 git commit -am "edits"
 git push heroku master
